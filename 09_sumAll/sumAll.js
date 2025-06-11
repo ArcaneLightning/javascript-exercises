5d39ce8a6045a4d973a6a336f5b1de1f50640c14
ArcaneLightningXXX <mehulbiala@gmail.com>
2025-06-11 12:01:39 -0700

    Finished array exercises

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (!(Number.isInteger(x) && x >= 0) || !(Number.isInteger(y) && y >= 0)) return "ERROR"
+    let sum = 0;
+    for (let i = Math.min(x, y); i <= Math.max(x, y); i++) {
+        sum += i;
+    }
+    return sum
 };
 
 // Do not edit below this line
